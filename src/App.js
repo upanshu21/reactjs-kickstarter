597e0fe29b8efacee6e5102249bd18d681375b01
upanshu21 <upanshu21@gmail.com>
2020-06-17 01:06:22 +0530

    component with media tag

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap'; //bootstrap implementation in react
+import Menu from './component/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Home</NavbarBrand>
+          </div>
+        </Navbar> 
+        <Menu dishes={this.state.dishes} />  
+      </div>                           
+    );
+  }
+
+}
+
+export default App;
